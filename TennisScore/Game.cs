8dc29873d4eabc725a12bfbeb79b72936b4d214c
詮華柏辰 <qaz989@chuanhwa.com.tw>
2018-04-16 14:53:12 +0800

    Love_All

--- TennisScore/Game.cs
+++ TennisScore/Game.cs
@@ -5,5 +5,10 @@
         public int SecondPlayerScore { get; set; }
         public int FirstPlayerScore { get; set; }
         public int Id { get; set; }
+
+        public string ScoreResult()
+        {
+            return "Love All";
+        }
     }
 }
